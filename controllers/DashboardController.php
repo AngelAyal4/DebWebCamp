797c012239cecb85817f4ee549079047f2aa9b44
AngelAyal4 <ajoseayala.00@gmail.com>
2025-06-23 04:02:16 -0300

    Inicio creacion de la vista de dashboard

---
+++ controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class DashboardController {
+
+    public static function index(Router $router){
+        // Render a la vista 
+        $router->render('admin/dashboard/index', [
+            'titulo' => 'Panel de Control'
+        ]);
+    }
+
+}
