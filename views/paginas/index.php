8fff790546297aca6531a5ee921ff00bf9c3a174
AngelAyal4 <ajoseayala.00@gmail.com>
2025-08-15 22:20:26 -0300

    toques finales a las animaciones del inicio

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -8,21 +8,24 @@
 
 <section class="resumen">
     <div class="resumen__grid">
-        <div class="resumen__bloque">
-            <p class="resumen__texto resumen__texto--numero"><?php echo $ponentes_total ?></p>
-            <p class="resumen__texto">Speakers
+        <div <?php aos_animacion(); ?> class="resumen__bloque">
+            <p class="resumen__texto resumen__texto--numero"><?php echo $ponentes_total; ?></p>
+            <p class="resumen__texto">Speakers</p>
         </div>
-        <div class="resumen__bloque">
-            <p class="resumen__texto resumen__texto--numero"><?php echo $conferencias_total ?></p>
-            <p class="resumen__texto">Conferencias
+
+        <div <?php aos_animacion(); ?> class="resumen__bloque">
+            <p class="resumen__texto resumen__texto--numero"><?php echo $conferencias_total; ?></p>
+            <p class="resumen__texto">Conferencias</p>
         </div>
-        <div class="resumen__bloque">
-            <p class="resumen__texto resumen__texto--numero"><?php echo $workshops_total ?></p>
-            <p class="resumen__texto">Workshops
+
+        <div <?php aos_animacion(); ?> class="resumen__bloque">
+            <p class="resumen__texto resumen__texto--numero"><?php echo $workshops_total; ?></p>
+            <p class="resumen__texto">Workshops</p>
         </div>
-        <div class="resumen__bloque">
-            <p class="resumen__texto resumen__texto--numero"><?php echo $asistentes_total ?></p>
-            <p class="resumen__texto">Asistentes
+
+        <div <?php aos_animacion(); ?> class="resumen__bloque">
+            <p class="resumen__texto resumen__texto--numero">500</p>
+            <p class="resumen__texto">Asistentes</p>
         </div>
     </div>
 </section>
@@ -31,18 +34,25 @@
 
 <section class="speakers">
     <h2 class="speakers__heading">Speakers</h2>
-    <p class="speakers__descripcion">Conoce a los expertos que compartirán sus conocimientos en DevWebCamp</p>
+    <p class="speakers__descripcion">Conoce a nuestros expertos de DevWebCamp</p>
+
     <div class="speakers__grid">
-        <?php foreach($ponentes as $ponente): ?>
-            <div class="speaker">
+        <?php foreach($ponentes as $ponente) { ?>
+            <div <?php aos_animacion(); ?> class="speaker">
                 <picture>
-                    <source srcset="img/speakers/<?php echo $ponente->imagen ?>.webp" type="image/webp">
-                    <source srcset="img/speakers/<?php echo $ponente->imagen ?>.png" type="image/png">
-                    <img class="speaker__imagen" loading="lazy" width="200" height="300" src="img/speakers/<?php echo $ponente->imagen ?>.png" alt="Imagen ponente">
+                    <source srcset="img/speakers/<?php echo $ponente->imagen; ?>.webp" type="image/webp">
+                    <source srcset="img/speakers/<?php echo $ponente->imagen; ?>.png" type="image/png">
+                    <img class="speaker__imagen" loading="lazy" width="200" height="300" src="img/speakers/<?php echo $ponente->imagen; ?>.png" alt="Imagen Ponente">
                 </picture>
+
                 <div class="speaker__informacion">
-                    <h4 class="speaker__nombre"><?php echo $ponente->nombre . ' ' . $ponente->apellido ?></h4>
-                    <p class="speaker__ubicacion"><?php echo $ponente->ciudad . ', ' . $ponente->pais ?></p>
+                    <h4 class="speaker__nombre">
+                        <?php echo $ponente->nombre . ' ' . $ponente->apellido; ?>
+                    </h4>
+
+                    <p class="speaker__ubicacion">
+                        <?php echo $ponente->ciudad . ', ' . $ponente->pais; ?>
+                    </p>
 
                     <nav class="speaker-sociales">
                         <?php
@@ -86,16 +96,17 @@
                         <?php } ?> 
                     </nav>
 
-                    <ul class="speaker__listado-skils">
+                    <ul class="speaker__listado-skills">
                         <?php 
                             $tags = explode(',', $ponente->tags);
-                            foreach($tags as $tag) { ?>
-                            <li class="speaker__skill"><?php echo $tag ?></li>
+                            foreach($tags as $tag) { 
+                        ?>
+                            <li class="speaker__skill"><?php echo $tag; ?></li>
                         <?php } ?>
                     </ul>
                 </div>
             </div>
-        <?php endforeach; ?>
+        <?php } ?>
     </div>
 </section>
 
@@ -106,25 +117,25 @@
 <!-- Valor de los tickets -->
 
 <section class="boletos">
-    <h2 class="boletos__heading">Boletos</h2>
-    <p class="boletos__descripcion">Precios de DevWebCamp</p>
+    <h2 class="boletos__heading">Boletos & Precios</h2>
+    <p class="boletos__descripcion">Precios para DevWebCamp</p>
 
     <div class="boletos__grid">
-        <div class="boleto boleto--presencial">
-            <h4 class="boleto__logo">&#60;DevWebCamp/></h4>
-            <h3 class="boleto__plan">Presencial</h3>
-            <p class="boleto__precio">$220</p>
+        <div <?php aos_animacion(); ?> class="boleto boleto--presencial">
+            <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
+            <p class="boleto__plan">Presencial</p>
+            <p class="boleto__precio">$199</p>
         </div>
 
-        <div class="boleto boleto--virtual">
-            <h4 class="boleto__logo">&#60;DevWebCamp/></h4>
-            <h3 class="boleto__plan">Virtual</h3>
-            <p class="boleto__precio">$60</p>
+        <div <?php aos_animacion(); ?> class="boleto boleto--virtual">
+            <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
+            <p class="boleto__plan">Virtual</p>
+            <p class="boleto__precio">$49</p>
         </div>
 
-        <div class="boleto boleto--gratis">
-            <h4 class="boleto__logo">&#60;DevWebCamp/></h4>
-            <h3 class="boleto__plan">Presencial</h3>
+        <div <?php aos_animacion(); ?> class="boleto boleto--gratis">
+            <h4 class="boleto__logo">&#60;DevWebCamp /></h4>
+            <p class="boleto__plan">Gratis</p>
             <p class="boleto__precio">Gratis - $0</p>
         </div>
     </div>
@@ -133,6 +144,5 @@
         <a href="/paquetes" class="boleto__enlace">Ver Paquetes</a>
     </div>
 </section>
-
 
 
